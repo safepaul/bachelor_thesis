b5f6303a88d32c02c73641d99e4fcfcb66cad373
Paul <work.sqm@pm.me>
2025-10-03 19:13:04 +0200

    Speed changes every 2 seconds, changed to malloc pointer

--- src/main/app.c
+++ src/main/app.c
@@ -6,26 +6,37 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "portmacro.h"
+#include "unistd.h"
 
 
 /* -------------------------------------------------------------------
  * Task function that prints the time once every 'sleep' seconds.
  * ------------------------------------------------------------------- */
 void taskPrintTime(void *tfParams){
-    // int sleep = *( (int *) tfParams );                              // No error checking
-    int sleep = ( (int)(uintptr_t) tfParams );                       // No error checking
+    int *sleep = (int *)tfParams;
     while (1) {
         printf("\n Current time: -- %lld --\n", time(NULL));        // (?) Not portable 
-        vTaskDelay( sleep / portTICK_PERIOD_MS);                    // No error checking
+        vTaskDelay( *sleep / portTICK_PERIOD_MS);                    // No error checking
     }
 }
 
 void app_main(void){
-    long period = 1000;
-    xTaskCreate(taskPrintTime,
+    // this will probably be handled by the MC initialized and MC manager in the future
+    int *period = malloc(sizeof(int));
+    *period = 1000;
+    xTaskCreatePinnedToCore(taskPrintTime,
                 "Print Time",
                 2048,
-                (void *)period,
+                period,
                 1,
-                NULL);
+                NULL,
+                0);
+
+    while (1) {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        *period = 100;
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        *period = 1000;
+    }
+
 }
