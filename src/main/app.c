25073278c36a08c7e18082c0f237abd4b3447440
Paul <work.sqm@pm.me>
2025-10-03 18:31:00 +0200

    Task creation test

--- src/main/app.c
+++ src/main/app.c
@@ -1,8 +1,31 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/idf_additions.h"
+#include "portmacro.h"
 
-int app_main(){
 
-    printf("AAAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAAAA\n");
+/* -------------------------------------------------------------------
+ * Task function that prints the time once every 'sleep' seconds.
+ * ------------------------------------------------------------------- */
+void taskPrintTime(void *tfParams){
+    // int sleep = *( (int *) tfParams );                              // No error checking
+    int sleep = ( (int)(uintptr_t) tfParams );                       // No error checking
+    while (1) {
+        printf("\n Current time: -- %lld --\n", time(NULL));        // (?) Not portable 
+        vTaskDelay( sleep / portTICK_PERIOD_MS);                    // No error checking
+    }
+}
 
-    return 0;
+void app_main(void){
+    long period = 1000;
+    xTaskCreate(taskPrintTime,
+                "Print Time",
+                2048,
+                (void *)period,
+                1,
+                NULL);
 }
