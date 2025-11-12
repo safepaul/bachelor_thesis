10195044bb69348234e09d3297577f7715b976ff
Pablo Saiz <work.sqm@pm.me>
2025-11-12 17:18:45 +0100

    Mode-Task API and structures laid out

--- components/mcmanager/include/mcmanager.h
+++ components/mcmanager/include/mcmanager.h
@@ -1,12 +1,77 @@
 #ifndef MCMANAGER_H
 #define MCMANAGER_H
 
+#include <stdint.h>
+
+
+
+
+//   -  TASK DEFINITION  -
+
+// structure defining the parameters of a task
+typedef struct {
+
+    uint16_t period;
+
+} task_params_t;
+
+
+// structure defining the relevant information of a task
+typedef struct task_info {
+
+    uint16_t id;
+    task_params_t params;
+    struct task_info *next;
+
+} task_info_t;
+
+
+//  -  MODE DEFINITION  -
+
+// structure defining the relevant information of a mode
+// `tasks` is a pointer to the first node of the task list
+typedef struct mode_info {
+
+    uint16_t id;
+    task_info_t *tasks;
+    struct mode_info *next;
+
+} mode_info_t;
+
+
+
+
+
+// Creates a mode in the mode list
+void mcm_create_mode(uint16_t mode_id);
+
+// Creates a new task and adds it to the end of the specified Mode
+void mcm_add_task_to_mode(uint16_t mode_id, uint16_t task_id, task_params_t task_params);
+
+//
+void mcm_create_transition();
+
+//
+void mcm_add_task_to_transition();
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  * This is the "master" function. Calling the mcr() triggers the rest of the functions.
  * 
  * */
 void mode_change_request();
+
+
 
 
 #endif // !MCMANAGER_H
--- components/mcmanager/src/mcmanager.c
+++ components/mcmanager/src/mcmanager.c
@@ -3,10 +3,30 @@
 #include "freertos/idf_additions.h"
 #include "stdlib.h"
 #include "esp_log.h"
+#include <stdint.h>
 
 
 // Component tag for ESP-IDF logging
 static const char* TAG = "MCMANAGER";
+
+
+
+void mcm_create_mode(uint8_t mode_id){
+
+    // data structure 
+    // i dont know how many entries(modes) there will be beforehand
+    // indexed by id (mode id, an integer)
+    // contains tasks with their parameters
+    // i dont know how many tasks in each mode
+    // i know the type and the amount of parameters of the tasks
+    // i know there will be < 100 modes
+    // i know there will be < 1000 tasks
+
+}
+
+
+
+
 
 
 
